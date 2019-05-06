6949a34a002a1a2e2db271e0fc848a03bfa1f1c5
hammer <>
2019-05-06 11:44:46 +0300

    test

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,23 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  // res.render('index', { title: 'Express' });
+    res.redirect('/home');
+});
+
+/* GET home page. */
+router.get('/home', function(req, res, next) {
+  res.render('home', { title: 'Express' });
+});
+
+/* GET home page. */
+router.get('/login', function(req, res, next) {
+  res.render('login', { title: 'Express' });
+});
+
+/* GET home page. */
+router.get('/signin', function(req, res, next) {
+  res.render('signin', { title: 'Express' });
 });
 
 module.exports = router;
