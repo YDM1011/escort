a4cb6950cfcaa36bcebd9e0fb4c1d290c8ea89a5
hammer <>
2019-05-13 16:35:58 +0300

    test

--- app/middlewares/isLoggedIn.js
+++ app/middlewares/isLoggedIn.js
@@ -6,7 +6,8 @@ module.exports = function (req, res, next) {
   if (req.jwt) {
       const jwt = require('jsonwebtoken');
       const protect = req.cookies['sid'] || req.jwt.token;
-        console.log(req.cookies['sid']);
+        console.log(req.cookies);
+        console.log(req.jwt);
       if(!protect){
           return res.forbidden("forbidden12");
       }
