c0fb73c111e3a492421fc1521c1d9a9253fcbed0
hammer <>
2019-05-13 11:13:34 +0300

    test

--- app/config/express.js
+++ app/config/express.js
@@ -69,7 +69,9 @@ const init = (app, config) =>{
 
 // catch 404 and forward to error handler
     app.use((req, res, next) => {
-        next(createError(404));
+        const err = new Error('Not Found');
+        err.status = 404;
+        next(err);
     });
 
 // error handler
